0ab61b33787e749dcda6d8cb07641fbcb35013bb
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-17 23:10:50 +0300

    count component created

---
+++ src/components/Count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="todos.length === 0">Todo not Entered!</div>
+  <div v-else>
+    <p><strong>Complated Todo:</strong>{{ trueCount }}</p>
+    <p><strong>Pending Todo:</strong>{{ falseCount }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  name: `Count`,
+  setup() {
+    const store = useStore();
+    const todos = computed(() => store.state.todos);
+    const trueCount = computed(() => store.getters.getDoneTrue);
+    const falseCount = computed(() => store.getters.getDoneFalse);
+    return {
+      todos,
+      trueCount,
+      falseCount,
+    };
+  },
+};
+</script>
+
+<style></style>
