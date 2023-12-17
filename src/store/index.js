3fa4835d49f4c119490bb53010c54cda21d8a495
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-17 23:12:07 +0300

    vuex file created, todo add and delete function added and localstorage added

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,46 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+
+const storedTodos = JSON.parse(localStorage.getItem("todos"));
 
 export default createStore({
   state: {
+    todos: storedTodos || [],
   },
   getters: {
+    getDoneTrue: (state) => {
+      return state.todos.filter((todo) => todo.done === true).length;
+    },
+    getDoneFalse: (state) => {
+      return state.todos.filter((todo) => todo.done === false).length;
+    },
   },
   mutations: {
+    setTodo: (state, todo) => {
+      state.todos.push(todo);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+    },
+    setToggleDone: (state, id) => {
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.done = !todo.done;
+        localStorage.setItem("todos", JSON.stringify(state.todos));
+      }
+    },
+    setDeleteTodo: (state, id) => {
+      state.todos = state.todos.filter((todo) => todo.id !== id);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+    },
   },
   actions: {
+    addTodo: ({ commit }, todo) => {
+      commit("setTodo", todo);
+    },
+    toggleDone: ({ commit }, id) => {
+      commit("setToggleDone", id);
+    },
+    deleteTodo: ({ commit }, id) => {
+      commit("setDeleteTodo", id);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
