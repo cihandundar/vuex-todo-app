3602124928f05f33e5a30409428c4c8d75afcd84
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-17 23:11:27 +0300

    todo page created

---
+++ src/views/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+  <div>
+    <add-todo />
+  </div>
+  <div>
+    <Count />
+  </div>
+  <div>
+    <list-todo />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo.vue";
+import Navbar from "../components/Navbar.vue";
+import Count from "@/components/Count.vue";
+import ListTodo from "@/components/ListTodo.vue";
+
+export default {
+  name: `Todo`,
+  components: {
+    Navbar,
+    AddTodo,
+    Count,
+    ListTodo,
+  },
+};
+</script>
+<style lang=""></style>
